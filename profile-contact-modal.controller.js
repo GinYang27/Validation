ac1f29902fb6d2f75f8eb53e6787efb47a8e1628
GinYang27 <gin.yang17@gmail.com>
2017-07-13 13:57:19 -0500

    Update profile-contact-modal.controller.js

--- profile-contact-modal.controller.js
+++ profile-contact-modal.controller.js
@@ -11,27 +11,27 @@
       ContactModalController.$inject = ['$scope','$uibModalInstance', 'userId', 'profileService', 'alertModalService'];
 
       function ContactModalController($scope, $uibModalInstance, userId, profileService, alertModalService) {
-        var self = this;
+            var self = this;
 
-      self.closeModal = closeModal;
-      self.send = send;
+            self.closeModal = closeModal;
+            self.send = send;
 
-      /**
-       * Close modal
-       */
-      function closeModal() {
-        $uibModalInstance.dismiss('cancel');
+            /**
+             * Close modal
+             */
+            function closeModal() {
+              $uibModalInstance.dismiss('cancel');
+            }
+
+            function send() {
+                  profileService.contact(userId, $scope.subject, $scope.content).then(success, failed);
+                  function success() {
+                        alertModalService.openAlertModal('success', true, 'Your message has been sent.');
+                        $uibModalInstance.dismiss('cancel');
+                  }
+                  function failed() {
+                        alertModalService.openAlertModal('error', true, 'Sorry, your message did not send successfully. Please try again');
+                  }	
+            }
       }
-
-      function send() {
-        profileService.contact(userId, $scope.subject, $scope.content).then(success, failed);
-        function success() {
-          alertModalService.openAlertModal('success', true, 'Your message has been sent.');
-          $uibModalInstance.dismiss('cancel');
-        }
-        function failed() {
-          alertModalService.openAlertModal('error', true, 'Sorry, your message did not send successfully. Please try again');
-        }	
-      }
-    }
 })();
